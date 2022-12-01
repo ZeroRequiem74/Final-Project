8294cc36b2e15d10135f23618bc0d6c24c98129e
ZeroRequiem74 <zerorequiem2474@gmail.com>
2022-12-01 13:34:28 -0700

    beginnings

--- Program.cs
+++ Program.cs
@@ -1,6 +1,30 @@
-﻿class Program{
+﻿using Raylib_cs;
+using System.Numerics;
 
-    static void Main(string[] args){
-        args;
+namespace HelloWorld{
+
+    static class Program{
+
+        public static void Main(string[] args){
+            var ScreenHeight = 480;
+            var ScreenWidth = 800;
+
+            Raylib.InitWindow(ScreenWidth, ScreenHeight, "GameObject");
+            Raylib.SetTargetFPS(60);
+
+            while (!Raylib.WindowShouldClose()){
+
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.EndDrawing();
+
+
+                Raylib.CloseWindow();
+            }
+
+            Raylib.CloseWindow();
+        }
     }
+
 }
